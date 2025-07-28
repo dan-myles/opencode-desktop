a8da60e9b7b816db5716c03a7ee9c6dec0d93018
dan-myles <79137382+dan-myles@users.noreply.github.com>
2025-07-28 13:14:26 -0500

    feat: new titlebar

---
+++ src/app/components/titlebar/index.tsx
@@ -0,0 +1,8 @@
+export function Titlebar() {
+  return (
+    <div
+      className="titlebar bg-background sticky top-0 z-50 flex h-8 items-stretch
+        select-none"
+    ></div>
+  )
+}
--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,9 +1,12 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 
+import { Titlebar } from "../components/titlebar"
+
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Titlebar />
       <Outlet />
       <TanStackRouterDevtools />
     </>
